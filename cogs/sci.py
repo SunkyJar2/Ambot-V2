bd6935ff83824dbf0eceea77046618f2e5c9aaf0
SunkyJar2 <119668029+SunkyJar2@users.noreply.github.com>
2023-04-14 13:54:47 +0700

    gtw

--- cogs/sci.py
+++ cogs/sci.py
@@ -1,12 +1,11 @@
 import random
 import discord
-import scipy
 from scipy import constants
 from discord.ext import commands
 from random import choice
 
 
-class Scipy(commands.Cog):  # create a class for our cog that inherits from commands.Cog
+class SciPy(commands.Cog):  # create a class for our cog that inherits from commands.Cog
     # this class is used to create a cog, which is a module that can be added to the bot
 
     def __init__(self, bot):  # this is a special method that is called when the cog is loaded
@@ -16,5 +15,5 @@ class Scipy(commands.Cog):  # create a class for our cog that inherits from comm
     async def litermeter(self, ctx, liter: float):
         await ctx.respond(constants.liter * liter)
 
-def setup(bot):  # this is called by Pycord to setup the cog
-    bot.add_cog(Scipy(bot))  # add the cog to the bot
+def setup(bot): # this is called by Pycord to setup the cog
+    bot.add_cog(SciPy(bot)) # add the cog to the bot
